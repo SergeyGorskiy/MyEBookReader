a86995fe0a4c88d80eb0b143db60b566c03b8c8d
Sergey Nazhestkin <9497020@mail.ru>
2021-09-22 22:29:50 +0300

    Доделан.

--- MyEBookReader/Program.cs
+++ MyEBookReader/Program.cs
@@ -11,6 +11,25 @@ namespace MyEBookReader
     {
         static void Main(string[] args)
         {
+            string[] words = CreateWordArray(@"http://www.gutenberg.org/files/54700/54700-0.txt");
+            Parallel.Invoke(() =>
+            {
+                Console.WriteLine("Begin first task...");
+                GetLongestWord(words);
+            },
+            () =>
+            {
+                Console.WriteLine("Begin second task...");
+                GetMostCommonWords(words);
+            },
+            () =>
+            {
+                Console.WriteLine("Begin third task...");
+                GetCountForWord(words, "oblomov");
+            });
+            Console.WriteLine("Returned from Parallel.Invoke");
+            Console.WriteLine("Press any key to exit");
+            Console.ReadKey();
         }
 
         static string[] CreateWordArray(string uri)
@@ -21,9 +40,35 @@ namespace MyEBookReader
                 StringSplitOptions.RemoveEmptyEntries);
         }
 
+        private static void GetLongestWord(string[] words)
+        {
+            var longestWord = words
+                .OrderByDescending(w => w.Length)
+                .Select(w => new {Name = w, Lenght = w.Length})
+                .First();
+            Console.WriteLine($"Task 1 -- The longest word is {longestWord.Name} -- {longestWord.Lenght} letters");
+        }
+        private static void GetMostCommonWords(string[] words)
+        {
+            var commonWords = words
+                .Where(w => w.Length > 6)
+                .GroupBy(w => w.ToLower())
+                .OrderByDescending(w => w.Count())
+                .Select(w => new {Name = w.Key, Count = w.Count()})
+                .Take(10);
+            
+            Console.WriteLine("Task 2 -- The most common words are:");
+            foreach (var w in commonWords)
+            {
+                Console.WriteLine($"\t{w.Name} -- {w.Count}");
+            }
+        }
+
         private static void GetCountForWord(string[] words, string term)
         {
-            var findWord = words.Where(w => w.ToUpper().Contains(term.ToUpper())).Select(w => w);
+            var findWord = words
+                .Where(w => w.ToLower().Contains(term.ToLower()))
+                .Select(w => w);
             Console.WriteLine($@"Task 3 -- The word ""{term}"" occurs {findWord.Count()} times");
         }
     }
